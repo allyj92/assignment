62060ad1536e7929446dae8b632d5ad694152a55
eunjaelim <imeunjae361@gmail.com>
2024-05-03 20:44:30 +0900

    2024-05-03

---
+++ class/src/StringExample02.java
@@ -0,0 +1,15 @@
+public class StringExample02 {
+    public static void main(String[] args){
+        String name1 = "Kim Coding";
+        String name;
+        name = "Kim coding";
+
+        String name2 = new String("Kim Coding");
+
+        System.out.println(name1 == name2);
+        System.out.println(name1 == name);
+
+
+
+    }
+}
---
+++ class/src/stringLength.java
@@ -0,0 +1,10 @@
+public class stringLength {
+    public static void main(String[] args){
+
+        String str = "codestates";
+
+        System.out.println(str.length());
+        System.out.println(str.indexOf('O')); // 없으면 1을 반환함
+        
+    }
+}
